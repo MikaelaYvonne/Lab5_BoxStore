83d5596d9e1a4820b7953efddc5f0b9326f4ee9e
amcnutt1996 <amcnutt1996@gmail.com>
2025-10-02 08:27:14 -0400

    Added NotShelfStable class

---
+++ src/NotShelfStable.java
@@ -0,0 +1,29 @@
+/**
+ * @author  Gregory McNutt
+ *          Section 001-003
+ *          Date 10/01/25
+ *          Purpost: Create NotShelfStable child class from FoodItem
+ */
+class NotShelfStable extends FoodItem {
+    /**
+     * Constructor for NotShelfStable class
+     *
+     * @param skuNumber unique identifier of items
+     * @param itemName  name of food item
+     * @param price     price of food item
+     * @param itemCount quantity of food item in stock
+     */
+    NotShelfStable(int skuNumber, String itemName, double price, int itemCount){
+        super(skuNumber, itemName, price, itemCount);
+    }
+
+// Behaviors / Methods
+
+    //Getters / Accessors
+
+
+    // Setters / Modifiers
+
+
+    //TODO: add override method to this class
+}
