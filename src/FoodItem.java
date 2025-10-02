c13df66dc422ce65bff9675a1316a41c2720e91b
Mikaela Yvonne Dacanay <mikaelayvonned@gmail.com>
2025-10-01 20:15:54 -0400

    calculateTax && getReturnPolicy for FoodItem

--- src/FoodItem.java
+++ src/FoodItem.java
@@ -7,4 +7,14 @@ public abstract class FoodItem extends StoreItem{
     public FoodItem(int skuNumber, String itemName, double price, int itemCount){
         super(skuNumber, itemName, price, itemCount);
     }
+
+    @Override
+    public double calculateTax() {
+        return getPrice() * FOOD_TAX; // lower tax for food
+    }
+
+    @Override
+    public String getReturnPolicy() {
+        return "Food items are non-refundable";
+    }
 }
