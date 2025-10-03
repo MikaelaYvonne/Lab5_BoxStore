357f7931788b0274877db3326f90b81293a6e098
amcnutt1996 <amcnutt1996@gmail.com>
2025-10-02 21:03:08 -0400

    Added a separate class for customer menus instead of including a bunch of stuff in the main method.

---
+++ src/CustomerMenus.java
@@ -0,0 +1,99 @@
+import com.sun.jdi.event.ExceptionEvent;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+//Custom class to throw our own exceptions for invalid data input by user.
+class CustomException extends Exception {
+    int value;
+    public CustomException (String message){
+        super(message);
+        }
+}
+
+public class CustomerMenus {
+    public static int showMainMenu(Scanner scanner){
+        int choice = 0;
+
+        //show user menu
+        while (choice == 0) {
+            System.out.println("~~~~Welcome to The Wilmington Quick Shop~~~~");
+            System.out.println("Please select an Option: ");
+            System.out.println("1. Buy Item");
+            System.out.println("2. Sell Item");
+            System.out.println("0. Exit");
+            try{
+                choice = scanner.nextInt();
+                if (choice != 0 && choice != 1 && choice != 2){
+                    throw new CustomException("Invalid Choice, please input a value from the menu options.");
+                }
+            } catch (InputMismatchException e){ //catches it if the scanner.nextInt() returns an error.
+                System.out.println("Please enter a valid option.");
+                scanner.next();
+            } catch (CustomException e) { //custom exception if the user doesn't input 1, 2, or 0
+                System.out.println(e.getMessage());
+            }
+
+            switch(choice){
+                case 0:
+                    System.exit(1);
+                    break;
+                case 1:
+                    showBuyMenu(scanner);
+                    break;
+                case 2:
+                    showSellMenu(scanner);
+                    break;
+            }
+        }
+        return choice;
+    }
+
+
+    public static void showBuyMenu(Scanner scanner){
+        int choice = 0;
+
+        while (choice == 0){
+            System.out.println("~~~~Time To Buy Stuff!~~~~");
+            System.out.println("What type of item would you like to buy?");
+            System.out.println("1. Food");
+            System.out.println("2. Household");
+            System.out.println("3. Electronics");
+            System.out.println("4. Clothing");
+            System.out.println("0. Go Back");
+
+            try{
+                choice = scanner.nextInt();
+            } catch (Exception e){
+                System.out.println("Please select an item on the menu");
+            }
+
+            switch (choice){
+                case 0:
+                    showMainMenu(scanner);
+                    break;
+                /**
+                 * Ok hear me out, what if we make a thing to grab the items from teh StoreItem array that we have
+                 * so that way we can not have to show like 50 bazillion menu's. We just use the buyMenu and
+                 * use it while working with the objects / data in the StoreItem classes so it works for everything.
+                 */
+//                case 1:
+                    //TODO: create a showBuyFoodMenu method to show options when buying food.
+//                    showBuyFoodMenu(scanner);
+//                    break;
+//                case 2:
+//                    showBuyHouseholdMenu(scanner);
+//                    break;
+//                case 3:
+//                    showBuyElectronicsMenu(scanner);
+
+
+            }
+        }
+    }
+
+    public static void showSellMenu(Scanner scanner){
+        //TODO: add stuff here to make it work like the one above :)
+    }
+
+}
--- src/WQSDacanayMcNuttWesley.java
+++ src/WQSDacanayMcNuttWesley.java
@@ -7,76 +7,10 @@ public class WQSDacanayMcNuttWesley {
         boolean terminate = false;
 
         while (!terminate){
-            int choice = showMainMenu(scanner);
+            int choice = CustomerMenus.showMainMenu(scanner);
             if (choice == 0){
                 terminate = true;
             }
         }
-
-
-
-    }
-    public static int showMainMenu(Scanner scanner){
-        int choice = 0;
-
-        while (choice == 0) {
-            System.out.println("~~~~Welcome to The Wilmington Quick Shop~~~~");
-            System.out.println("Please select an Option: ");
-            System.out.println("1. Buy Item");
-            System.out.println("2. Sell Item");
-            System.out.println("0. Exit");
-            try{
-                choice = scanner.nextInt();
-            } catch (Exception e){
-                System.out.println("Please enter a menu option.");
-            }
-            switch(choice){
-                case 0:
-                    System.exit(1);
-                    break;
-                case 1:
-                    showBuyMenu(scanner);
-            }
-        }
-        return choice;
-    }
-
-
-    public static void showBuyMenu(Scanner scanner){
-        int choice = 0;
-
-        while (choice == 0){
-            System.out.println("~~~~Time To Buy Stuff!~~~~");
-            System.out.println("What type of item would you like to buy?");
-            System.out.println("1. Food");
-            System.out.println("2. Household");
-            System.out.println("3. Electronics");
-            System.out.println("4. Clothing");
-            System.out.println("0. Go Back");
-
-            try{
-                choice = scanner.nextInt();
-            } catch (Exception e){
-                System.out.println("Please select an item on the menu");
-            }
-
-            switch (choice){
-                case 0:
-                    showMainMenu(scanner);
-                    break;
-//                case 1:
-//                    showBuyFoodMenu(scanner);
-//                    break;
-//                case 2:
-//                    showBuyHouseholdMenu(scanner);
-//                    break;
-//                case 3:
-//                    showBuyElectronicsMenu(scanner);
-
-
-            }
-
-        }
-
     }
 }
