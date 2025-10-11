11f6862b001a34755726bb28db28eadc30ca3155
Mikaela Yvonne Dacanay <mikaelayvonned@gmail.com>
2025-10-10 20:34:29 -0400

    Laptop Class - no docu

---
+++ src/Laptop.java
@@ -0,0 +1,26 @@
+/**
+ * @author  Mikaela Yvonne Dacanay
+ *          Section 001-003
+ *          Date 10/01/25
+ *          Purpose:
+ */
+
+
+public class Laptop extends ElectronicsItem {
+    private double screenSize;
+    private int ramGB;
+
+    public Laptop(int skuNumber, String itemName, double price, int itemCount, String brand, int warrantyMonths, double screenSize, int ramGB) {
+        super(skuNumber, itemName, price, itemCount, brand, warrantyMonths);
+        this.screenSize = screenSize;
+        this.ramGB = ramGB;
+    }
+
+    public double getScreenSize() { return screenSize; }
+    public int getRamGB() { return ramGB; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Screen: " + screenSize + "\", RAM: " + ramGB + "GB";
+    }
+}
