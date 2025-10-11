a75d245628e9e29fc3cf8b90b5cdcb485206ac41
Mikaela Yvonne Dacanay <mikaelayvonned@gmail.com>
2025-10-10 21:12:01 -0400

    ClothingItem class extension: Shoe, Shirt, Outerwear created

--- src/Outerwear.java
+++ src/Outerwear.java
@@ -1,2 +1,24 @@
-public class Outerwear {
+/**
+ * @author  Mikaela Yvonne Dacanay
+ *          Section 001-003
+ *          Date 10/01/25
+ *          Purpose:
+ */
+
+
+public class Outerwear extends ClothingItem{
+    private boolean isWaterproof; // I cant think of a different identifier lol
+
+    public Outerwear(int skuNumber, String itemName, double price, int itemCount,
+                     String size, String color, boolean isWaterproof){
+        super(skuNumber, itemName, price, itemCount, size, color);
+        this.isWaterproof = isWaterproof;
+    }
+
+    public boolean isWaterproof() { return isWaterproof; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Waterproof: " + isWaterproof;
+    }
 }
--- src/Shoe.java
+++ src/Shoe.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+/**
+ * @author  Mikaela Yvonne Dacanay
+ *          Section 001-003
+ *          Date 10/01/25
+ *          Purpose:
+ */
 
-public class Shoe {
+
+public class Shoe extends ClothingItem {
+    private String style; // sneakers, boots, heels etc
+
+    public Shoe(int skuNumber, String itemName, double price, int itemCount,
+                String size, String color, String style){
+        super(skuNumber, itemName, price, itemCount, size, color);
+        this.style = style;
+    }
+
+    public String getStyle() { return style; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Style: " + style;
+    }
+
 }
