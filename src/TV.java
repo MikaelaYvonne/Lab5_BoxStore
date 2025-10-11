9306d35eeae73f446dd187a339b89389e43433c3
Mikaela Yvonne Dacanay <mikaelayvonned@gmail.com>
2025-10-10 20:41:42 -0400

    Phone and TV Class created - no docu

---
+++ src/TV.java
@@ -0,0 +1,28 @@
+/**
+ * @author  Mikaela Yvonne Dacanay
+ *          Section 001-003
+ *          Date 10/01/25
+ *          Purpose:
+ */
+
+
+public class TV extends ElectronicsItem {
+
+    private double screenSize;
+    private boolean isSmart;
+
+    public TV(int skuNumber, String itemName, double price, int itemCount, String brand, int warrantyMonths, double screenSize, boolean isSmart) {
+        super(skuNumber, itemName, price, itemCount, brand, warrantyMonths);
+        this.screenSize = screenSize;
+        this.isSmart = isSmart;
+    }
+
+    public double getScreenSize() { return screenSize; }
+    public boolean isSmart() { return isSmart; }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Screen: " + screenSize + "\", Smart: " + isSmart;
+    }
+}
+
