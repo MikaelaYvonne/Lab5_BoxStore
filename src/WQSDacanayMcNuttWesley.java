297ebb7ed80f023deb4db488fdadda9038d347dd
amcnutt1996 <amcnutt1996@gmail.com>
2025-10-17 10:25:29 -0400

    added documentation

--- src/WQSDacanayMcNuttWesley.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.Scanner;
-
-public class WQSDacanayMcNuttWesley {
-    public static void main(String[] args) {
-        new MainWindow();
-    }
-}
---
+++ src/WQSDacanayMcNuttWesley_GUI.java
@@ -0,0 +1,10 @@
+/**
+ * @author Gregory McNutt
+ * Date:    17 Oct 2025
+ * Purpose: main driver class to run the other windows for the GUI functionality.
+ */
+public class WQSDacanayMcNuttWesley_GUI {
+    public static void main(String[] args) {
+        new MainWindow();
+    }
+}
